d33581555855d62d988c777764ed9c4c45d38d6f
PiKeyAr <pikeyar@pm.me>
2025-02-02 17:56:38 +0900

    Add functions to copy and move folders

--- SADXModManager/Utils.cs
+++ SADXModManager/Utils.cs
@@ -1041,5 +1041,48 @@ namespace SADXModManager
 			// If the folder doesn't exist, nothing to be done
 			return false;
 		}
+
+		/// <summary>Copies a folder from one location to another.</summary>
+		static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+		{
+			// https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+
+			// Get information about the source directory
+			var dir = new DirectoryInfo(sourceDir);
+
+			// Check if the source directory exists
+			if (!dir.Exists)
+				throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+			// Cache directories before we start copying
+			DirectoryInfo[] dirs = dir.GetDirectories();
+
+			// Create the destination directory
+			Directory.CreateDirectory(destinationDir);
+
+			// Get the files in the source directory and copy to the destination directory
+			foreach (FileInfo file in dir.GetFiles())
+			{
+				string targetFilePath = Path.Combine(destinationDir, file.Name);
+				file.CopyTo(targetFilePath);
+			}
+
+			// If recursive and copying subdirectories, recursively call this method
+			if (recursive)
+			{
+				foreach (DirectoryInfo subDir in dirs)
+				{
+					string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+					CopyDirectory(subDir.FullName, newDestinationDir, true);
+				}
+			}
+		}
+
+		/// <summary>Moves a folder from one location to another. This is done because Directory.Move produces an IO error when the source and destination paths aren't on the same volume.</summary>
+		public static void MoveDirectory(string src, string dest)
+		{
+			CopyDirectory(src, dest, true);
+			Directory.Delete(src, true);
+		}
 	}
 }
