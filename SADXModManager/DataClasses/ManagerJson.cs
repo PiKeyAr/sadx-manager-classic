1bd4e657951414eafdeb82b4a38831c0d9f3177a
PiKeyAr <pikeyar@pm.me>
2025-02-02 17:56:59 +0900

    Add new manager JSON struct for import

---
+++ SADXModManager/DataClasses/ManagerJson.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+// Manager.json in SAManager.
+// Only the bare minimum is implemented for reading the SADX game folder from the new SA Mod Manager settings file.
+
+namespace SADXModManager.DataClasses
+{
+	public class GameEntry
+	{
+		public enum GameType
+		{
+			Unsupported = 0,
+			SADX = 1,
+			SA2 = 2,
+		}
+
+		// Game name such as "Sonic Adventure DX"
+		public string Name { get; set; }
+
+		// Full path to the game folder
+		public string Directory { get; set; }
+
+		// Game EXE name, such as "sonic.exe"
+		public string Executable { get; set; }
+
+		// SADX or SA2
+		public GameType Type { get; set; }
+
+		public GameEntry() { }
+	}
+
+	public class ManagerJson
+	{
+		public List<GameEntry> GameEntries { get; set; }
+	}
+}
